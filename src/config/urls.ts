fe5b9cac5576ed532f408c732dc226eff69b143b
naturexie <786281870@qq.com>
2026-03-02 10:24:09 +0800

    feat: update urls

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,6 +1,6 @@
 export const WEB_URLS = {
-  EARN: "https://solana-mobile.jimi-db0.workers.dev/",
-  BORROW: "https://solana-mobile.jimi-db0.workers.dev/solana-wallet-test.html",
-  SWAP: "https://x.rhea.finance/trade",
-  MY_EARN: "https://app.rhea.finance/portfolio",
+  EARN: "https://multi-chain-lending-cx3.pages.dev/supply",
+  BORROW: "https://multi-chain-lending-cx3.pages.dev/borrow",
+  SWAP: "https://multi-chain-lending-cx3.pages.dev/trade",
+  MY_EARN: "https://multi-chain-lending-cx3.pages.dev/my",
 };
