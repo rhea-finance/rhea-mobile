30d3ff89e4d514425b072f567538c43606d0277e
naturexie <786281870@qq.com>
2026-03-02 11:53:28 +0800

    feat: update WEB_URLS

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,6 +1,6 @@
 export const WEB_URLS = {
-  EARN: "https://multi-chain-lending-cx3.pages.dev/supply",
-  BORROW: "https://multi-chain-lending-cx3.pages.dev/borrow",
-  SWAP: "https://multi-chain-lending-cx3.pages.dev/trade",
-  MY_EARN: "https://multi-chain-lending-cx3.pages.dev/my",
+  EARN: "https://top.app.rhea.finance/supply",
+  BORROW: "https://top.app.rhea.finance/borrow",
+  SWAP: "https://top.app.rhea.finance/trade",
+  MY_EARN: "https://top.app.rhea.finance/my",
 };
